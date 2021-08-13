a73fd22eec1ca6324857bb7cd9174d831b2127af
gahyeon <rkgus8955@likelion.org>
2021-08-13 19:46:29 +0900

    템플릿 상속

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'firstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['firstproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
