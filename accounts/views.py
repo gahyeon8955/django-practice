3e21cfa1ffbde5fb1918e6f63c47209997d41673
gahyeon8955 <rkgus8955@likelion.org>
2021-08-16 20:16:46 +0900

    회원가입 수정

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-# Create your views here.
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -17,6 +16,7 @@ def signup(request):
             return render(request, 'signup.html', {'error': 'Passwords must match'})
     else:
         return render(request, 'signup.html')
+
 
 def login(request):
     return render(request, 'login.html')
