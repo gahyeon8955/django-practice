a06def61f5784694141f87abbacd705c99e63a0b
gahyeon8955 <rkgus8955@likelion.org>
2021-08-16 21:02:11 +0900

    로그인,로그아웃

--- accounts/views.py
+++ accounts/views.py
@@ -19,4 +19,18 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error':'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
